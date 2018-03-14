baf3e334f86f86cd0e1137559a3190c6d43744dc
Unknown <roger.levy@gmail.com>
2018-03-14 00:59:23 -0400

    Add 3! and 4!

--- utils.f
+++ utils.f
@@ -3,6 +3,9 @@
 : 2+!  swap over cell+ +! +! ;
 : 3@  dup @ swap cell+ dup @ swap cell+ @ ;
 : 4@  dup @ swap cell+ dup @ swap cell+ dup @ swap cell+ @ ;
+: 3!  dup >r  2 cells + !  r> 2! ;
+: 4!  dup >r  2 cells + 2! r> 2! ;
+
 
 \ Pen
 create penx  0 ,  here 0 ,  constant peny
