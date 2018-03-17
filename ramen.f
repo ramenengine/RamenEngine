e820067d63cbb88d0466e92e8cceaa025df4b70b
Unknown <roger.levy@gmail.com>
2018-03-17 17:53:28 -0400

    Update path refs

--- ramen.f
+++ ramen.f
@@ -1,14 +1,14 @@
-include kit/lib/version.f
+include kit/ans/version.f
 $000100 [version] ramen-version
-
+\ --------------------------------------------------------------------------------------------------
 $000600 include kit/kit.f
 include ramen/utils.f
 include ramen/struct.f
 include ramen/fixops.f
-include kit/lib/sf/fixedp.f \ we need fixed-point literals ... it's unavoidable
+include kit/plat/sf/fixedp.f \ must come after fixops.  we need fixed-point literals ... it's unavoidable
 include ramen/image.f
+\ --------------------------------------------------------------------------------------------------
 include ramen/obj.f
-
-redef on
 include ramen/node.f
+\ --------------------------------------------------------------------------------------------------
 redef off  \ from here on fields only defined if not previously defined
