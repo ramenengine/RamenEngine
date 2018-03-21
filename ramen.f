1d36ce8abf6e20ff24242b009eec7fc204c6ba67
Unknown <roger.levy@gmail.com>
2018-03-20 22:09:44 -0400

    Include ramen/assets, add fixed point DISPLAYWH and VIEWWH (scale compensated)

--- ramen.f
+++ ramen.f
@@ -8,8 +8,17 @@ include ramen/fixops.f
 include kit/plat/sf/fixedp.f \ must come after fixops.  we need fixed-point literals ... it's unavoidable
 include ramen/image.f
 include ramen/color.f
+include ramen/assets.f
 \ --------------------------------------------------------------------------------------------------
 include ramen/obj.f
 include ramen/node.f
 \ --------------------------------------------------------------------------------------------------
 redef off  \ from here on fields only defined if not previously defined
+
+: displayw  display al_get_display_width 1p ;
+: displayh  display al_get_display_height 1p ;
+: displaywh  displayw displayh ;
+
+: vieww  displayw global-scale 1p / ;
+: viewh  displayh global-scale 1p / ;
+: viewwh  vieww viewh ;
