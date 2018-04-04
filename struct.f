380a5c68a0691df4e2e0937b8037b6eac31f3782
Roger Levy <roger.levy@gmail.com>
2018-04-04 00:36:38 -0400

    Delete old comment from struct.f

--- struct.f
+++ struct.f
@@ -1,5 +1,4 @@
 
-\ Basic structs; forward-compatible with smallfry, but valtypes are thrown away
 : sfield  ( struct bytes valtype -- <name> )  ( adr -- adr+n )
     drop create over @ ,  swap +!  does> @ + ;
 : svar  cell swap sfield ;
