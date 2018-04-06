bc9244fdfbeb403d30f4ae3b6ddfce7f41240cb8
Roger Levy <roger.levy@gmail.com>
2018-04-06 02:00:23 -0400

    Add task.f and exflies.f (both untested)

---
+++ lib/task.f
@@ -0,0 +1,90 @@
+\ Multitasking for game objects
+
+\ The following words should only be used within a task:
+\  PAUSE END FRAMES SECS
+\ The following words should never be used within a task:
+\  - External calls
+\  - Console output 
+\  (See below for a workaround mechanism)
+
+var sp  var rp  12 cells field ds  12 cells field rs
+
+create main  object  \ proxy for the Forth data and return stacks
+
+\ RAMEN version:
+\  Uses NXT to go to next object.
+\  To avoid using the tiny return stack of the tasks, we don't use objlists' counts, and instead rely on ME being 0 to break.
+\  Therefore you cannot call MULTI on a pool, only a plain objlist.
+
+\ important internal note: this word must not CALL anything or use the return stack. (why?)
+: pause
+    \ save state
+    dup \ ensure TOS is on stack
+    sp@ sp !
+    rp@ rp !
+    \ look for next task.  rp=0 means no task.  end of list = jump to main task and resume that
+    begin  nxt  me if  rp @  else  main as  true  then  until
+    \ restore state
+    rp @ rp!
+    sp @ sp!
+    drop \ ensure TOS is in TOS register
+;
+
+: end    remove pause ;
+: halt   begin pause again ;
+: pauses 0 do pause loop ;
+: secs   fps * pauses ;  \ not meant for precision timing
+
+\ external-calls facility - say "['] word later" to schedule a word that calls an external library.
+\ you can pass a single parameter to each call, such as an object or an asset.
+\ NOTE: you don't have to consume the parameter, and as a bonus, you can leave as much as you want
+\ on the stack.
+
+\ TBD
+\ 20000 cellstack queue
+\ : later  ( val xt -- )  swap queue push queue push ;
+\ : arbitrate
+\     queue scount swap a!>  do  sp@ >r  i 2@ execute  r> sp!  2 cells +loop
+\     queue 0 truncate ;
+: arbitrate ;
+: later  2drop ;
+
+\ pulse the multitasker.
+: multi  ( objlist -- )
+    {
+        ol.first @ main link !
+        dup
+        sp@ main 's sp !
+        rp@ main 's rp !
+        main as  ['] pause catch
+        ?dup if
+            main as  rp @ rp! sp @ sp!  ( drop <--- why was this here? )  throw
+        then
+        drop
+    } 
+    { arbitrate }
+;
+
+
+: self?  sp@ ds >=  sp@ rs <= and ;
+
+decimal
+    : perform> ( n -- <code> )
+        self? if    ds 10 cells + sp!  r>  rs 10 cells + rp!  >r  exit
+              else  ds 10 cells + !  ds 9 cells + sp !  r> rs 10 cells + !  rs 10 cells + rp !
+                    ['] halt >code rs 11 cells + !
+              then ;
+
+    : perform  ( xt n obj -- )
+        { as
+        ds 10 cells + !
+        ds 9 cells + sp !
+        >code rs 10 cells + !
+        ['] halt >code rs 11 cells + !
+        rs 10 cells + rp !
+        }
+    ;
+fixed
+
+: direct  ( obj -- <word> )  '  0  rot  perform ;
+: direct:  ( obj -- ... code ... ; )  :noname  [char] ; parse evaluate  0  rot  perform ;
