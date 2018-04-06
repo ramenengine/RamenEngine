9621785235ab866734bf99d2e86556bf5b3fff2b
Roger Levy <roger.levy@gmail.com>
2018-04-05 23:03:40 -0400

    add ramen/brick.f

---
+++ brick.f
@@ -0,0 +1,12 @@
+\ This loads Ramen plus kinda your "standard library set"
+\ Very general purpose
+
+include kit/ans/version.f
+$000100 [version] brick-ver
+$000100 include ramen/ramen
+$000100 include ramen/lib/rangetools
+$000100 include ramen/lib/zsort
+$000100 include ramen/lib/v2d
+$000100 include ramen/lib/draw
+\ $000100 include ramen/lib/cgrid
+\ $000100 include ramen/lib/task
