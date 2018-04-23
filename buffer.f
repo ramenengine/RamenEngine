5a084b207b323b0cb7910b4111e3099321d85549
Roger Levy <roger.levy@gmail.com>
2018-04-23 14:05:23 -0400

    Add BUFFER:

---
+++ buffer.f
@@ -0,0 +1,13 @@
+assetdef %buffer
+    %buffer int svar buffer.data
+    %buffer int svar buffer.size
+
+: recreate-buffer  ( buffer -- )
+    >r  r@ buffer.size @ allocate throw  r> buffer.data ! ;
+
+: init-buffer  ( size buffer -- )
+    >r  dup r@ buffer.size !  allocate throw  r@ buffer.data !
+    ['] recreate-buffer r> register ;
+
+: buffer:   ( size -- <name> )
+    create  %buffer sizeof buffer  init-buffer ;
