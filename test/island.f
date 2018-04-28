1b17b9dd8dd8df07ef88e83063ffad0c43a8f88a
Unknown <roger.levy@gmail.com>
2018-04-28 19:36:31 -0400

    update call to loadtilemap in island.f

--- test/island.f
+++ test/island.f
@@ -6,7 +6,7 @@ $10000 include ramen/tiled/tiled
 
 only forth definitions also xmling also tmxing
 
-map 0 tileset drop constant ts constant tsdom
+map 0 tileset[] drop constant ts constant tsdom
 
 map " Ground" layer  0 0 tilebuf loc  tilebuf pitch@  readlayer
-map " Ground" layer  0 0 load-tilemap \ same thing but also converts the data
+map " Ground" layer  0 0 loadtilemap \ same thing but also converts the data
