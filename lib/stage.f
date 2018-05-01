438b95b755a5d440bc8eb06220b8ba401d129f16
Roger Levy <roger.levy@gmail.com>
2018-05-01 12:19:06 -0400

    Make SUBJECT a DEFER

--- lib/stage.f
+++ lib/stage.f
@@ -6,9 +6,11 @@ $000100 [version] stage-ver
 \ Note that you should put scrolling tilemaps in a different objlist, move them around to offset
 \ the global scrolling, and/or replace the renderer or they will scroll too fast and be clipped.
 
-0 value subject
+defer subject
 0 value cam
 objlist stage
+
+:is subject 0 ;
 
 
 \ -----------------------------------------------------------------------
@@ -17,13 +19,13 @@ objlist stage
 : track
     ?dup -exit
     cam 0= if drop exit then
-    's x 2@  .s viewwh  2halve  2-  cam 's x 2! ;
+    's x 2@  viewwh  2halve  2-  cam 's x 2! ;
 
 : camtrans
     cam -exit
     m1 al_identity_transform
-    m1 global-scale 1af dup al_scale_transform
     m1  cam 's x 2@ 2pfloor 2negate 2af  al_translate_transform
+    m1 #globalscale s>f 1sf dup al_scale_transform
     m1 al_use_transform ;
 
 
