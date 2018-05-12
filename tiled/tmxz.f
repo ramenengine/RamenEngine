8b74dd5c6b28adea091b5d3712e4b60ac090532b
Roger Levy <roger.levy@gmail.com>
2018-05-12 14:40:25 -0400

    Add DECOMPRESS (untested)

--- tiled/tmxz.f
+++ tiled/tmxz.f
@@ -3,4 +3,7 @@
 
 : layersize ( layer-nnn -- #bytes# )  wh@ cells ;
 : compressed?  ( layer-nnn -- flag )
+    dup " compression" attr? not if  drop  false  exit then
+    " compression" val " zlib" compare 0= ?dup ?exit
+    -1 abort" Error in COMPRESSED? : Only zlib is supported!" ;
 
