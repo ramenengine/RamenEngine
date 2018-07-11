49dc99aee4285c17fb1e420ff28bb63d586b5a00
Unknown <roger.levy@gmail.com>
2018-07-11 01:11:12 -0400

    simplify sprites.f
    fasdfasdf
    
    
    jhgf
    
    
    jhgf
    
    
    fasdf

--- lib/sprites.f
+++ lib/sprites.f
@@ -11,6 +11,7 @@ redef on
     var img  var anm  var rgntbl  var anmspd  var ctr  \ don't change the order of the first four...
 redef off
 
+
 \ right now sx and sy need to be initialized to 1,1 , and tint needs to be initialized to 1,1,1,1
 \ this is just until we add prototypes to obj.f ...
 : /scalerot  1 1 sx 2!  1 1 1 1 tint 4! ;
@@ -21,47 +22,44 @@ redef off
 \ --------------------------------------------------------------------------------------------------
 \ Drawing!
 
-: objsubimage  ( image n flip -- )  \ uses image subdivision feature
-    >r swap afsubimg
-        tint 4@ 4af  cx 2@ at@  4af  sx 3@ 3af  r>
+: sprite ( image srcx srcy w h flip )  \ pass a rectangle defining the region to draw
+    locals| flip h w y x img |  img >bmp ?exit
+    img image.bmp @  x y w h 4af  tint 4@ 4af  cx 2@ at@  4af  sx 3@ 3af  flip
         al_draw_tinted_scaled_rotated_bitmap_region ;
 
-
-: objregion ( image rect flip )  \ pass a rectangle defining the region to draw
-    locals| flip rect img |  img >bmp ?exit
-    img image.bmp @  rect 4@ 4af  tint 4@ 4af  cx 2@ at@  4af  sx 3@ 3af  flip
-        al_draw_tinted_scaled_rotated_bitmap_region ;
+: objsubimage  ( image n flip -- )  >r  over >subxywh  r> sprite ;
 
 \ --------------------------------------------------------------------------------------------------
-\ Define region tables.
+\ Region tables.
 
-: region,  ( x y w h )  4,  0 , 0 , ;
-: <origin  ( x y )  2negate  here 2 cells - 2! ;
 6 cells constant /region
+    \ x , y , w , h , originx , originy , 
 
 \ --------------------------------------------------------------------------------------------------
 \ Define animations
 
 3 cells constant /frame
-: anim[  0 0 at  here ;
+    \ index+flip , ofsx , ofsy , 
+    \ hflip = $1
+    \ vflip = $2
+    \ index is fixed point
+
 : anim:  ( image speed regiontable|0 -- loopaddr )  ( -- )  \ when defined word is called, animation plays
-    create  3,  anim[  does>  @+ swap @+ anmspd ! @+ rgntbl ! swap animate ;
-: frame,  ( index+flip -- )  , at@ 2, ;
-: frames,  0 do dup frame, loop drop  ;
-: [h]  #1 or ;
-: [v]  #2 or ;
+    create  3,  here
+    does>  @+ swap @+ anmspd ! @+ rgntbl ! swap animate ;
+: frames,  for  3dup 3, loop 3drop  ;
 : loop:  drop here ;
 : ;anim  ( loopaddr -- )  here -  $deadbeef ,  , ;
 
-: animrange:  ( start len -- <name> )
-    anim:  over + swap do  i frame,  loop  ;anim ;
+: animrange,  ( start len -- ) over + swap do  i , 0 , 0 ,  loop  ;
+: addanim:  ( cellstack -- cellstack loopaddr ) here over push here ;
 
-: animtable[  ( var size -- addr )  cellstack dup rot ! ;
-: ]animtable  drop ;
-: subanim[  here over push  anim[ ;
-: ]subanim  ;anim ;
 
-: frame@  ( -- srcx srcy w h )
+\ --------------------------------------------------------------------------------------------------
+\ Drawing and pulsing animation
+
+\ Get current frame
+: curframe  ( -- srcx srcy w h )
     img @ anm @ or 0= if  0 0 16 16  exit then
     rgntbl @ ?dup if
         anm @ @  /region * +  4@
@@ -69,21 +67,13 @@ redef off
         anm @ @  img @  >subxywh
     then ;
 
-
-\ --------------------------------------------------------------------------------------------------
-\ Drawing and pulsing animation
-
 action animlooped ( -- )
 
+\ Draw + animate
 : animspr  ( -- )
     anm @ -exit  img @ -exit
     anm @ cell+ 2@ +at  \ apply the offset
-    img @
-        rgntbl @ ?dup if
-            anm @ @ dup >r  /region * +  r>  #3 and  objregion
-        else
-            anm @ @ dup #3 and   objsubimage
-        then
+    img @  curframe  anm @ @ #3 and  sprite
     anmspd @ ctr +!
     begin  ctr @ 1 >= while
         ctr --  /frame anm +!
