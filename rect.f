9469d1126ee95ea15e461aed416fcd7b16f5af7f
Unknown <roger.levy@gmail.com>
2018-07-30 16:34:46 -0400

    fix X2! Y2! and XY2!

--- rect.f
+++ rect.f
@@ -15,6 +15,6 @@ struct rect
 : rect.  dup xy@ 2. rot wh@ 2. ;
 
 : xywh@  4@ ;                     : xywh! 4! ;
-: x2@   dup x@ swap w@ + ;        : x2@   >r r@ x@ - r> w! ;
-: y2@   dup y@ swap h@ + ;        : y2@   >r r@ y@ - r> h! ;
-: xy2@  dup 2@ rot wh@ 2+ ;       : xy2@  >r r@ xy@ 2- r> wh! ;
+: x2@   dup x@ swap w@ + ;        : x2!   >r r@ x@ - r> w! ;
+: y2@   dup y@ swap h@ + ;        : y2!   >r r@ y@ - r> h! ;
+: xy2@  dup 2@ rot wh@ 2+ ;       : xy2!  >r r@ xy@ 2- r> wh! ;
