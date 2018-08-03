fc2eed8d530d6968469cf6cc7fcbce537fae78d1
Unknown <roger.levy@gmail.com>
2018-08-03 02:34:24 -0400

    fix RECREATE-BUFFER2D

--- lib/buffer2d.f
+++ lib/buffer2d.f
@@ -1,15 +1,17 @@
 require ramen/lib/array2d.f
 
 decimal
-: recreate-buffer2d  ( buffer2d -- )
+: init-buffer2d  ( array2d -- )
     >r  r@ array2d.cols 2@ 2i i* cells allocate throw  r> array2d.data ! ;
+: recreate-buffer2d  ( asset -- )
+    /assetheader + init-buffer2d ;
 fixed
 
 : init-buffer2d  ( cols rows buffer2d -- )
     >r udup r@ array2d.cols 2!
     cells r@ array2d.pitch !
     ['] recreate-buffer2d r@ /assetheader - register
-    r> recreate-buffer2d ;
+    r> init-buffer2d ;
 
 : buffer2d:   ( cols rows -- <name> )  ( -- array2d )
     create  /assetheader /allot  %array2d sizeof allotment  init-buffer2d
