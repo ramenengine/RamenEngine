4e2a858853cd8ba71245005b74733b7fc7a9461e
Unknown <roger.levy@gmail.com>
2018-08-15 19:48:56 -0400

    compiles but doesn't run
    font is not loading right

--- ide/ide.f
+++ ide/ide.f
@@ -1,3 +1,5 @@
+\ SwiftForth only
+
 \ Simpler IDE, library-style.
 \  This is a semi-one-way street.
 \  (For now, we're using SwiftForth's PERSONALITY facility to save
@@ -6,7 +8,7 @@
 \ go> words:
 \   REPL  ( -- )  processes commandline events.
 \ show> words:
-\   .CMDLINE  ( -- )  displays the commandline at given pen position and font, including the current idiom and the stack
+\   .CMDLINE  ( -- )  displays the commandline at given pen position and font, including the stack
 \   .OUTPUT  ( -- )  displays commandline output
 \ misc words:
 \   GO  ( -- )  starts the default REPL interface, no game displayed.  calls RASA
@@ -19,12 +21,13 @@
 \   OUTPUT  ( -- variable )  variable that stores the bitmap to draw text output onto.
 \   /S  reset the Forth stack
 
+require afkit/plat/win/clipb.f
 
-variable consolas
-consolas value cmdfont
-private:  0  xvar x  xvar y  4 cells xfield colour  struct /cursor
-public:
-create cursor  /cursor /allot
+define ideing
+
+s" ramen/ide/data/consolab.ttf" #20 ALLEGRO_TTF_NO_KERNING font: consolas
+create cursor 6 cells /allot
+: colour 2 cells + ;
 variable scrolling  scrolling on
 create cmdbuf #256 /allot
 create history  #256 /allot
@@ -35,103 +38,92 @@ create attributes
   0.3 , 1 , 0.3 , 1 ,  \ green
   1 , 1 , 0.3 ,   1 ,  \ light yellow
   0 , 1 , 1 ,     1 ,  \ cyan
-transform baseline
 variable output   \ output bitmap
-
-bu:
-create margins  0 , 0 , 0 , 0 ,   \ margins for the command history. (rectangle)
-ide:
+create margins  4 cells /allot   \ margins for the command history. (rectangle)
 
 \ --------------------------------------------------------------------------------------------------
 \ low-level stuff
-: fw  cmdfont @ fontw ;
-: fh  cmdfont @ fonth ;
+: fw  consolas chrw ;
+: fh  consolas chrh ;
 : cols  fw * ;
 : rows  fh * ;
-\ : #cmdrows  margins @h 1 - fh / pfloor ;
-: rm  margins @x2  displayw min ;
-: bm  margins @y2  displayh min ;
-: lm  margins @x  displayw >= if  0  else  margins @x  then ;
-: tm  margins @y  displayh >= if  0  else  margins @y  then ;
+: rm  margins x2@  displayw min ;
+: bm  margins y2@  displayh min ;
+: lm  margins x@  displayw >= if  0  else  margins x@  then ;
+: tm  margins y@  displayh >= if  0  else  margins y@  then ;
 : ?call  ?dup -exit call ;
 : ?.catch  ?dup -exit .catch  postpone [ ;
 : recall  history count cmdbuf place ;
 : store   cmdbuf count history place ;
 : typechar  cmdbuf count + c!  #1 cmdbuf c+! ;
 : rub       cmdbuf c@  #1 -  0 max  cmdbuf c! ;
-: paste     @clipb  #1 -  cmdbuf append ;
-: copy      cmdbuf count !clipb ;
-: ?paused  pause @ if  -timer  0 +to lag   else  +timer  then ;
+: paste     clipb@  #1 -  cmdbuf append ;
+: copy      cmdbuf count clipb! ;
+\ : ?paused  pause @ if  -timer  0 +to lag   else  +timer  then ;
 : keycode  evt ALLEGRO_KEYBOARD_EVENT.keycode @ ;
 : unichar  evt ALLEGRO_KEYBOARD_EVENT.unichar @ ;
-private:
-  : ctrl?  evt ALLEGRO_KEYBOARD_EVENT.modifiers @ ALLEGRO_KEYMOD_CTRL and ;
-  : alt?  evt ALLEGRO_KEYBOARD_EVENT.modifiers @ ALLEGRO_KEYMOD_ALT and ;
-public:
-: /margins 0 0 displayw displayh 3 rows - margins !xywh ;
-: 4@af  4@ 4af ;
-: /output  nativew nativeh 2i al_create_bitmap  output ! ;
+: ctrl?  evt ALLEGRO_KEYBOARD_EVENT.modifiers @ ALLEGRO_KEYMOD_CTRL and ;
+: alt?  evt ALLEGRO_KEYBOARD_EVENT.modifiers @ ALLEGRO_KEYMOD_ALT and ;
+: /margins  0 0 displaywh 3 rows - margins xywh! ;
+: /output  native 2@ al_create_bitmap output ! ;
 
 \ --------------------------------------------------------------------------------------------------
 \ Output words
-private:
-    : get-xy  ( -- #col #row )  cursor x 2@  lm tm 2-  fw fh 2/ 2i ;
-    : at-xy   ( #col #row -- )  2s>p fw fh 2*  lm tm 2+  cursor x 2! ;
-    : fill  ( w h bitmap -- )  onto  write-rgba blend>  rectf ;
-    : clear  ( w h bitmap -- )  0 0 0 0 color  fill ;
-    : outputw  rm lm - ;
-    : outputh  bm tm - ;
-    : get-size  ( -- cols rows )  outputw outputh fw fh 2/ 2i ;
-    : scroll
-        write-rgba blend>  output @ onto  0 -1 rows at  untinted  output @ blit
-        \ lm bm 1 rows - at   outputw 1 rows output @ clear
-        -1 rows cursor y +!
-    ;
-    : cr
-        lm cursor x !
-        1 rows cursor y +!
-        scrolling @ -exit
-        cursor y @ bm >= if  scroll  then
-    ;
-    : (emit)
-        ch c!
-        cursor x 2@ at  ch #1 print
-        fw cursor x +!
-        cursor x @ rm >= if  cr  then
-    ;
-    decimal
-        : emit  output @ onto  write-rgba blend>  cmdfont @ font>  cursor colour 4@ color  (emit) ;
-        : type  output @ onto  write-rgba blend>  cmdfont @ font>  cursor colour 4@ color  bounds  do  i c@ (emit)  loop ;
-        : ?type  ?dup if type else 2drop then ;
-    fixed
-    : attribute  s>p 4 cells * attributes +  cursor colour  4 imove ;
+: ramen-get-xy  ( -- #col #row )  cursor xy@  lm tm 2-  fw fh 2/ 2i ;
+: ramen-at-xy   ( #col #row -- )  2p fw fh 2*  lm tm 2+  cursor xy! ;
+: fill  ( w h bitmap -- )  onto>  write-rgba blend>  rectf ;
+: clear  ( w h bitmap -- )  black 0 alpha  fill ;
+: outputw  rm lm - ;
+: outputh  bm tm - ;
+: ramen-get-size  ( -- cols rows )  outputw outputh fw fh 2/ 2i ;
+: scroll
+    write-rgba blend>  output @ onto>  0 -1 rows at  untinted  output @ blit
+    -1 rows cursor y+!
+;
+: ramen-cr
+    lm cursor x!
+    1 rows cursor y+!
+    scrolling @ -exit
+    cursor y@ bm >= if  scroll  then
+;
+: (emit)
+    ch c!
+    cursor xy@ at  ch #1 print
+    fw cursor x+!
+    cursor x@ rm >= if  ramen-cr  then
+;
+decimal
+    : ramen-emit  output @ onto>  write-rgba blend>  consolas fnt !  cursor colour 4@ rgba  (emit) ;
+    : ramen-type  output @ onto>  write-rgba blend>  consolas fnt !  cursor colour 4@ rgba  bounds  do  i c@ (emit)  loop ;
+    : ramen-?type  ?dup if type else 2drop then ;
+fixed
+: ramen-attribute  1p 4 cells * attributes +  cursor colour  4 imove ;
 
-    : wipe  output @ onto  0 0 0 0 4af al_clear_to_color  0 0 at-xy ;
+: wipe  0 0 0 0 output @ clearbmp  0 0 ramen-at-xy ;
 
-    : zero  0 ;
+: zero  0 ;
 
-    create console-personality
-      4 cells , #19 , 0 , 0 ,
-      ' noop , \ INVOKE    ( -- )
-      ' noop , \ REVOKE    ( -- )
-      ' noop , \ /INPUT    ( -- )
-      ' emit , \ EMIT      ( char -- )
-      ' type , \ TYPE      ( addr len -- )
-      ' ?type , \ ?TYPE     ( addr len -- )
-      ' cr , \ CR        ( -- )
-      ' wipe , \ PAGE      ( -- )
-      ' attribute , \ ATTRIBUTE ( n -- )
-      ' zero , \ KEY       ( -- char )  \ not yet supported
-      ' zero , \ KEY?      ( -- flag )  \ not yet supported
-      ' zero , \ EKEY      ( -- echar ) \ not yet supported
-      ' zero , \ EKEY?     ( -- flag )  \ not yet supported
-      ' zero , \ AKEY      ( -- char )  \ not yet supported
-      ' 2drop , \ PUSHTEXT  ( addr len -- )  \ not yet supported
-      ' at-xy ,  \ AT-XY     ( x y -- )
-      ' get-xy , \ GET-XY    ( -- x y )
-      ' get-size , \ GET-SIZE  ( -- x y )
-      ' drop , \ ACCEPT    ( addr u1 -- u2)  \ not yet supported
-public:
+create ide-personality
+  4 cells , #19 , 0 , 0 ,
+  ' noop , \ INVOKE    ( -- )
+  ' noop , \ REVOKE    ( -- )
+  ' noop , \ /INPUT    ( -- )
+  ' ramen-emit , \ EMIT      ( char -- )
+  ' ramen-type , \ TYPE      ( addr len -- )
+  ' ramen-?type , \ ?TYPE     ( addr len -- )
+  ' ramen-cr , \ CR        ( -- )
+  ' wipe , \ PAGE      ( -- )
+  ' ramen-attribute , \ ATTRIBUTE ( n -- )
+  ' zero , \ KEY       ( -- char )  \ not yet supported
+  ' zero , \ KEY?      ( -- flag )  \ not yet supported
+  ' zero , \ EKEY      ( -- echar ) \ not yet supported
+  ' zero , \ EKEY?     ( -- flag )  \ not yet supported
+  ' zero , \ AKEY      ( -- char )  \ not yet supported
+  ' 2drop , \ PUSHTEXT  ( addr len -- )  \ not yet supported
+  ' ramen-at-xy ,  \ AT-XY     ( x y -- )
+  ' ramen-get-xy , \ GET-XY    ( -- x y )
+  ' ramen-get-size , \ GET-SIZE  ( -- x y )
+  ' drop , \ ACCEPT    ( addr u1 -- u2)  \ not yet supported
 
 \ --------------------------------------------------------------------------------------------------
 \ Command management
@@ -143,18 +135,21 @@ public:
 
 \ --------------------------------------------------------------------------------------------------
 \ Input handling
+
+: toggle  dup @ not swap ! ;
+
 : special  ( n -- )
   case
     [char] v of  paste  endof
     [char] c of  copy   endof
-    [char] p of  pause toggle  endof
+    [char] p of  pause toggle endof
   endcase ;
 
 : idekeys
-    \ always processed:
+    \ always:
     etype ALLEGRO_EVENT_DISPLAY_RESIZE =
-    etype EVENT_FULLSCREEN = or if
-        /margins  \ /output
+    etype FULLSCREEN_EVENT = or if
+        /margins
     then
     etype ALLEGRO_EVENT_KEY_DOWN = if
         keycode dup #37 < if  drop exit  then
@@ -182,65 +177,57 @@ public:
     then
 ;
 
-\ --------------------------------------------------------------------------------------------------
+\ ----------5----------------------------------------------------------------------------------------
 \ Rendering
 : .S2 ( ? -- ? )
   #3 attribute
-  DEPTH 0> IF DEPTH s>p  0 ?DO S0 @ I 1 + CELLS - @ . LOOP THEN
+  DEPTH 0> IF DEPTH 1p  0 ?DO S0 @ I 1 + CELLS - @ . LOOP THEN
   DEPTH 0< ABORT" Underflow"
   FDEPTH ?DUP IF
     ."  F: "
     0  DO  I' I - #1 - FPICK N.  #1 +LOOP
   THEN ;
 : +blinker interact @ -exit  #frames 16 and -exit  s[ [char] _ c+s ]s ;
-: .idiom   'idiom @ ?dup if  #4 attribute  body> >name count #1 - type  then  [char] > emit ;
-: .cmdbuf  #0 attribute  cmdfont @ font>  white  cmdbuf count +blinker print ;
+: .cmdbuf  #0 attribute  consolas fnt !  white  cmdbuf count +blinker print ;
 : bar      outputw  displayh bm -  black  output @ fill ;
-: .output  untinted  output @ blit ;
+: .output  output @ blit ;
 
 
 \ --------------------------------------------------------------------------------------------------
-\ redefined STEP> ... makes keyboard state be automatically cleared every frame when interact is on.
+\ redefined STEP> ... makes keyboard state be cleared every frame when interact is on.
 0 value 'idestep
 : ?clearkb  interact @ if clearkb then ;
-: step>  ( -- <code> )  r> to 'idestep  step>  ?clearkb 'idestep call ;
+: step>  ( -- <code> )  r> to 'idestep  step>  ?clearkb  'idestep call ;
 
 \ --------------------------------------------------------------------------------------------------
-global
-    : wipe  page ;
-    : /s  S0 @ SP! ;
-ide:
 \ --------------------------------------------------------------------------------------------------
-\ "API"
-
-function: al_load_ttf_font  ( zfilename size flags -- font )
-#1 constant ALLEGRO_TTF_NO_KERNING
-#2 constant ALLEGRO_TTF_MONOCHROME
+\ bring it all together
 
 : bottom  lm bm ;
-: .output   untinted  output @ blit ;
+: .output   output @ blit ;
 : .cmdline
     output @ >r  display al_get_backbuffer output !
-    get-xy 2>r  at@ cursor x 2!  bar  scrolling off  .s2  cr  .idiom  .cmdbuf  scrolling on   2r> at-xy
+    get-xy 2>r  at@ cursor xy!  bar  scrolling off  .s2  cr  .cmdbuf  scrolling on   2r> at-xy
     r> output !
 ;
 : /cmdline
     /s
-    \ z" afkit/dev/data/consolas16.png" al_load_bitmap_font  consolas !
-    soft
-    z" afkit/dev/data/consolab.ttf" #20 ALLEGRO_TTF_NO_KERNING al_load_ttf_font  consolas !
     /output
     1 1 1 1 cursor colour 4!
     /margins
     ['] >display is >ide  \ >IDE is redefined to take us to the display
     interact on
-    console-personality open-personality
+    ide-personality open-personality
 ;
-: ?repl  idekeys ;
-: rasa-system  idekeys ;
-: shade  0 0 0 0.3 color  0 0 at  displaywh rectf  white ;
-: rasa-overlay  interact @ if  shade  0 0 at  .output  bottom at  .cmdline  then ;
-: rasa  ['] rasa-system  is  ?system  ['] rasa-overlay  is ?overlay ;
-: go  /cmdline  rasa  " ld autoexec" ['] evaluate catch ?.catch  begin ok again ;
+: ide-system  idekeys ;
 
-gild
+: shade  black 0.3 alpha  0 0 at  displaywh rectf  white ;
+: ide-overlay  interact @ if  shade  0 0 at  .output  bottom at  .cmdline  then ;
+: rasa  ['] ide-system  is  ?system  ['] ide-overlay  is ?overlay ;
+
+: ide  /cmdline  rasa  s" ld autoexec" ['] evaluate catch ?.catch  begin go again ;
+
+
+only forth definitions
+: wipe  page ;
+: /s  S0 @ SP! ;
