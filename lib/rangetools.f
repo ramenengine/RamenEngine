7b586e6a7416a0f0e4ac7ed9ec6a8ee3e28eb337
Unknown <roger.levy@gmail.com>
2018-08-18 20:05:03 -0400

    add AREA

--- lib/rangetools.f
+++ lib/rangetools.f
@@ -1,4 +1,4 @@
-
+: area  ( x y w h -- x1 y1 x2 y2 )  2over 2+ ;
 : lowerupper  ( n n -- lower higher )  2dup > if  swap  then ;
 : between  ( n n -- n )  lowerupper  over -  #1 +  rnd + ;
 : 1clamp  ( n -- n )  0.0 1.0 clamp ;
