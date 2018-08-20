198aff4787ef0c50ed45082b6ac34bf30ffb2186
Unknown <roger.levy@gmail.com>
2018-08-20 12:36:34 -0400

    rename dev.f to main.f and move test/ out

--- test/sticker-knight/objects/gemBlueStroked.f
+++
@@ -1,5 +0,0 @@
-cr .( gemBlueStroked script loaded! )
-
-[defined] :tmx [if]
-    :tmx gemBlueStroked   ( nnn -- )  cr . ." COIN TMX RECIPE EXECUTED!!!" ;
-[then]
--- test/sticker-knight/objects/hero.f
+++
@@ -1,5 +0,0 @@
-cr .( Hero script loaded! )
-
-[defined] :tmx [if]
-    :tmx hero   ( nnn -- )  cr . ." HERO TMX RECIPE EXECUTED!!!" ;
-[then]
