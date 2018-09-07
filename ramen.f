679f2f6c2f39c70ad3d60e3265caa485ca9efb12
Unknown <roger.levy@gmail.com>
2018-09-07 16:32:53 -0400

    add color cycling background

--- ramen.f
+++ ramen.f
@@ -39,7 +39,10 @@ redef off  \ from here on fields only defined if not previously defined
 
 objlist stage  \ default object list
 
+\ --------------------------------------------------------------------------------------------------
+\ some graphics tools for the default engine state
 
+\ draw a rectangular vertical gradient 
 create gv  4 /ALLEGRO_VERTEX * /allot
 create gi  0 , #1 , #2 , #3 , 
 : v!  ( x y a n ) /ALLEGRO_VERTEX *  + >r  2af  r> 2! ;
@@ -50,14 +53,27 @@ create gi  0 , #1 , #2 , #3 ,
     c1 gv 2dup 0 color! 1 color!  c2 gv 2dup 2 color! 3 color!
     gv 0 0 gi #4 ALLEGRO_PRIM_TRIANGLE_FAN al_draw_indexed_prim ;
 
+\ convert lch, hsl, hsv to rgb
+\ hue is in degrees
+create (fc)  3 cells allot
+: !color  >r  (fc) color.r sf@ f>p  (fc) color.g sf@ f>p  (fc) color.b sf@ f>p  r> 3! ;
+: lch! ( l c h color -- )  >r  >rad  3af  (fc) dup cell+ dup cell+  al_color_lch_to_rgb  r> !color ;
+: hsl! ( h s l color -- )  >r  3af  (fc) dup cell+ dup cell+  al_color_hsl_to_rgb  r> !color ;
+: hsv! ( h s v color -- )  >r  3af  (fc) dup cell+ dup cell+  al_color_hsv_to_rgb  r> !color ;    
+\ --------------------------------------------------------------------------------------------------
 
+\ default engine state
 create (c1)  0.0 , 0 , 0.1 , 1 ,
 create (c2)  0.25 , 0.1 , 0.4 , 1 , 
-
+: colorcycle
+    0 0.1 frmctr -20 / (c1) lch!
+    0.25 0.4 frmctr -20 / (c2) lch!
+    (c1) (c2)
+;
 :noname
     show>
-        unmount  0 0 at  (c1) (c2) displaywh vgradient  mount
-        stage each> draw
+        unmount  0 0 at  colorcycle displaywh vgradient  
+        mount    stage each> draw
 ; execute
 
 : think  stage each> act ;
