e2472e4751b7b68a9a457ee2751711256710fcd8
Unknown <roger.levy@gmail.com>
2018-09-07 15:53:34 -0400

    gradient
    asdf

--- ramen.f
+++ ramen.f
@@ -2,27 +2,25 @@ include afkit/ans/version.f
 #1 0 0 [version]   
 \ always check the version
 
+\ The following is loaded once per session:
 [undefined] ramen [if] 
-true constant ramen
-
-\ --------------------------------------------------------------------------------------------------
-
-#1 0 0 include afkit/afkit.f  \ AllegroForthKit
-
-include ramen/variables.f
-include ramen/plat.f
-include afkit/dep/zlib/zlib.f
-include ramen/struct.f
-include ramen/color.f
-include ramen/fixops.f
-include afkit/plat/sf/fixedp.f \ must come after fixops.  we need fixed-point literals ... it's unavoidable
-include ramen/stack.f
-include ramen/rect.f
-include ramen/res.f
-
+    true constant ramen
+    
+    #1 0 0 include afkit/afkit.f  \ AllegroForthKit
+    
+    include ramen/variables.f
+    include ramen/plat.f
+    include afkit/dep/zlib/zlib.f
+    include ramen/struct.f
+    include ramen/color.f
+    include ramen/fixops.f
+    include afkit/plat/sf/fixedp.f \ must come after fixops.  we need fixed-point literals ... it's unavoidable
+    include ramen/stack.f
+    include ramen/rect.f
+    include ramen/res.f
 [then]
-\ The following always needs to be reloaded every time EMPTY is called:
 
+\ The following needs to be reloaded every time EMPTY is called:
 
 \ Assets
 include ramen/assets.f
@@ -39,8 +37,35 @@ redef off  \ from here on fields only defined if not previously defined
 
 : frmctr  frmctr 1p ;
 
+objlist stage  \ default object list
+
+
+create gv  4 /ALLEGRO_VERTEX * /allot
+create gi  0 , #1 , #2 , #3 , 
+: v!  ( x y a n ) /ALLEGRO_VERTEX *  + >r  2af  r> 2! ;
+: color! ( color a n )  /ALLEGRO_VERTEX *  + >r  4@ 4af  r> ALLEGRO_VERTEX.r 4! ;
+: vgradient  ( color1 color2 w h -- )
+    at@ 2+ at@ locals| y x y2 x2 c2 c1 |
+    x y gv 0 v!   x2 y gv 1 v!  x2 y2 gv 2 v!  x y2 gv 3 v!
+    c1 gv 2dup 0 color! 1 color!  c2 gv 2dup 2 color! 3 color!
+    gv 0 0 gi #4 ALLEGRO_PRIM_TRIANGLE_FAN al_draw_indexed_prim ;
+
+
+create (c1)  0.0 , 0 , 0.1 , 1 ,
+create (c2)  0.25 , 0.1 , 0.4 , 1 , 
+
 :noname
-    show>      0e 0e 0.5e 1e 4sf al_clear_to_color
-; execute  
+    show>
+        unmount  0 0 at  (c1) (c2) displaywh vgradient  mount
+        stage each> draw
+; execute
+
+: think  stage each> act ;
+: physics  stage each>  vx 2@ x 2+! ;
+
+:noname
+    step>  think  physics
+; execute
+
 
 only forth definitions marker (empty)
