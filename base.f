5805bac61bd50e852a4910cf51801a62d5218c94
Unknown <roger.levy@gmail.com>
2018-09-19 21:36:26 -0400

    refactor ramen.f, base.f, session.f

--- base.f
+++ base.f
@@ -1,6 +1,13 @@
+exists ramen [if] \\ [then]
+true constant ramen
+#1 0 0 include afkit/afkit.f  \ AllegroForthKit
+
+\ Low-level
 include ramen/variables.f
 include ramen/plat.f
+[undefined] LIGHTWEIGHT [if]
 include afkit/dep/zlib/zlib.f
+[then]
 include ramen/struct.f
 include ramen/color.f
 include ramen/fixops.f
@@ -9,5 +16,22 @@ include ramen/stack.f
 include ramen/rect.f
 include ramen/res.f
 
-: frmctr  frmctr 1p ;
+\ Assets
+include ramen/assets.f
+include ramen/image.f
+include ramen/font.f
+include ramen/buffer.f
+include ramen/sample.f
 
+\ Higher level facilities
+include ramen/obj.f
+include ramen/publish.f
+
+redef off  \ from here on fields only defined if not previously defined
+
+: frmctr  frmctr 1p ;
+objlist stage  \ default object list
+used @ value baseline
+: -stage  stage -objlist ;
+: empty  -stage -assets baseline used ! empty ;
+only forth definitions marker (empty)
--- ramen.f
+++ ramen.f
@@ -1,29 +1,14 @@
 exists empty [if] empty [then]
 include afkit/ans/version.f
 #1 0 0 [version]
-exists ramen [if] \\ [then]
-true constant ramen
-#1 0 0 include afkit/afkit.f  \ AllegroForthKit
 include ramen/base.f
-
-\ Assets
-include ramen/assets.f
-include ramen/image.f
-include ramen/font.f
-include ramen/buffer.f
-include ramen/sample.f
-
-\ Higher level facilities
-include ramen/obj.f
-include ramen/publish.f
-
-redef off  \ from here on fields only defined if not previously defined
-
-objlist stage  \ default object list
-used @ value baseline
-: -stage  stage -objlist ;
-: empty  -stage -assets baseline used ! empty ;
-
-include ramen/default.f
+[undefined] LIGHTWEIGHT [if]
+    include ramen/default.f
+[THEN]
 only forth definitions marker (empty)  \ gild point for empty
-only forth definitions
+ide-loaded @ not [if]
+    [in-platform] sf [if]
+        include ramen/ide/ide.f   \ gilded
+        s" test.f" file-exists [if]  ld test  [then]
+    [then]
+[then]
