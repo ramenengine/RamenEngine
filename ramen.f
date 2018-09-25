11818277fade43b36e6f3e2ff2849e71e5fe4cfd
Unknown <roger.levy@gmail.com>
2018-09-24 21:08:50 -0400

    1.5.0

--- ramen.f
+++ ramen.f
@@ -1,6 +1,6 @@
 [defined] [ramen] [if] \\ [then]
 include afkit/ans/version.f
-#1 #4 #0 [version] [ramen]
+#1 #5 #0 [version] [ramen]
 include ramen/base.f          \ gilded
 [in-platform] sf [if]
     include ramen/ide/ide.f   \ gilded
