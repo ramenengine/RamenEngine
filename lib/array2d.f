e539414b651da3da72e417c7f2a4fef6c043700f
Unknown <roger.levy@gmail.com>
2018-10-10 11:54:27 -0400

    move node.f and some array2d words to bucket/
    asdf
    
    
    asdf

--- lib/array2d.f
+++ lib/array2d.f
@@ -8,13 +8,7 @@ fixed
   loop ;
 
 : clip  ( col row #cols #rows #destcols #destrows -- col row #cols #rows )
-  2>r  2over 2+  0 0 2r@ 2clamp  2swap  0 0 2r> 2clamp  2swap 2over 2- ;
-
-: batch  ( ... addr #cells xt -- ... )  ( ... addr -- ... )
-  >code -rot  cells bounds do  i swap dup >r call  r>  cell +loop  drop ;
-
-: `batch  ( ... addr xt -- ... )  ( ... addr -- ... )  \ -1 is terminator
-  >code begin  over @ 0 >=  while  2dup 2>r  call  r> r> cell+ swap repeat  2drop ;
+    2>r  2over 2+  0 0 2r@ 2clamp  2swap  0 0 2r> 2clamp  2swap 2over 2- ;
 
 struct %array2d
     %array2d int svar array2d.cols
@@ -22,7 +16,8 @@ struct %array2d
     %array2d int svar array2d.pitch
     %array2d int svar array2d.data
     
-: array2d-head,  ( cols rows -- )  udup  2pfloor 2,  cells ,  here cell+ , ;
+: array2d-head,  ( cols rows -- )
+    udup  2pfloor 2,  cells ,  here cell+ , ;
 
 decimal
 \ by default the data field is set to the adjacent dictionary space
@@ -43,40 +38,26 @@ fixed
 \      if dest col/row are negative, we need to adjust the source start address!!
 
 : (clip)   ( col row #cols #rows array2d -- col row #cols #rows array2d  )
-  dims@ 1 1 2- clip ;
+    dims@ 1 1 2- clip ;
 
 : loc  ( col row array2d -- addr )
-  (clamp) >r  r@ array2d.pitch @ * swap cells +  r> array2d.data @ + ;
+    (clamp) >r  r@ array2d.pitch @ * swap cells +  r> array2d.data @ + ;
 
-: pitch@  ( array2d -- /pitch stride )  array2d.pitch @ ;
+: pitch@  ( array2d -- pitch )  array2d.pitch @ ;
 
-: addr-pitch  ( col row array2d -- addr /pitch )  dup >r loc r> array2d.pitch @ ;
-
-: write2d  ( src-addr pitch destcol destrow #cols #rows dest -- )
-    locals| dest |
-  dest (clip)  2swap dest addr-pitch  2swap  swap cells 2move ;
-
-: move2d  ( srcrow srccol destcol destrow #cols #rows src -- )
-  locals| src |
-  2>r 2>r  src addr-pitch  2r> 2r> write2d ;
+\ itteration
+: addr-pitch  ( col row array2d -- addr /pitch )
+    dup >r loc r> array2d.pitch @ ;
 
 : some2d  ( ... col row #cols #rows array2d XT -- ... )  ( ... addr #cells -- ... )
-  >r >r  r@ (clip)   2swap r> addr-pitch
-  r> locals| xt pitch src #rows #cols |
-  #rows 0 do  src #cols xt execute  pitch +to src  loop ;
-
+    >r >r  r@ (clip)   2swap r> addr-pitch
+    r> locals| xt pitch src #rows #cols |
+    #rows 0 do  src #cols xt execute  pitch +to src  loop ;
+    
 : some2d>  r> code> some2d ;
 
 :noname  third ifill ;
 : fill2d  ( val col row #cols #rows array2d -- )  literal some2d  drop ;
-
-: scan2d  ( ... array2d xt -- ... )  ( ... addr #cells -- ... )
-  >r >r  0  0  r@ dims@  r> r> some2d ;
-
-: scan2d>  r> code> scan2d ;
-
-\ : some2d>  r> code> some2d ;
-\ : each2d>  r> code> each2d ;
 
 :noname  cr  cells bounds do  i @ h.  cell +loop ;
 : 2d.  >r 0 0 r@ dims@ 16 16 2min  r> literal some2d  ;
