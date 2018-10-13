02aae9ea07c856cf12e133e0c00acd4eda3fdf44
Unknown <roger.levy@gmail.com>
2018-10-12 23:42:32 -0400

    update upscale.f

--- lib/upscale.f
+++ lib/upscale.f
@@ -1,4 +1,4 @@
+depend ramen/lib/draw.f
 nativewh canvas: canv
-
-: (upscale) canv >bmp onto> unmount call ;
-: upscale>  ( -- < code> )  r> (upscale)  mount  canv >bmp blit ;
+: (upscale)  canv >bmp onto>  unmount call  ;
+: upscale>  ( -- <code> )  r>  (upscale)  mount  0 0 at  canv >bmp blit ;
