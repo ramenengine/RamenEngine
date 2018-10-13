45bde811ad7491f0bbf34f1efd767c5d325bf020
Unknown <roger.levy@gmail.com>
2018-10-13 16:19:10 -0400

    upscale> now draws 3d content correctly and clears the canvas to 0,0,0,0

--- lib/upscale.f
+++ lib/upscale.f
@@ -1,4 +1,6 @@
 depend ramen/lib/draw.f
 nativewh canvas: canv
-: (upscale)  canv >bmp onto>  unmount call  ;
-: upscale>  ( -- <code> )  r>  (upscale)  mount  0 0 at  canv >bmp blit ;
+
+: (size)  viewwh canv resize-canvas ;
+: (upscale)  canv >bmp onto>  black 0 alpha backdrop  unmount call ;
+: upscale>  ( -- <code> )  (size)  r>  (upscale)  mount  0 0 at  canv >bmp blit ;
