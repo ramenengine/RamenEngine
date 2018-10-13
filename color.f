3996548f9db2436041e951bcd27558e3431255d4
Unknown <roger.levy@gmail.com>
2018-10-12 23:41:26 -0400

    rename color to %color

--- color.f
+++ color.f
@@ -1,7 +1,6 @@
-
-struct color
-    color 0 svar color.r
-    color 0 svar color.g
-    color 0 svar color.b
-    color 0 svar color.a
+struct %color
+    %color 0 svar color.r
+    %color 0 svar color.g
+    %color 0 svar color.b
+    %color 0 svar color.a
 
