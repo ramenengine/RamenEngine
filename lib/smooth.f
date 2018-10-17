07f03f9525590c8ba3a4acc246837467aad59c44
Unknown <roger.levy@gmail.com>
2018-10-17 02:07:45 -0400

    add lib/pjoy.f

--- lib/smooth.f
+++ lib/smooth.f
@@ -3,7 +3,7 @@ defer cb
 : each-data  ( xt adr -- )  ( ... adr -- ... )
     swap is cb  begin @ ?dup while  dup >r  cell+ cb   r> repeat ;
 : add-data  ( adr -- )  dup @ here rot ! , ;
-: @execute  dup @ execute ;
-: +smoother  ( xt -- )  smoothers add-data , ;
+: @execute  dup @ execute  ;
+: +smoother  ( xt -- )  ( adr -- )  smoothers add-data , ;
 : smooths  ( objlist )  each> ['] @execute smoothers each-data ;
 : lerp!  ( adr dest-val factor -- )  >r  over @ swap r> lerp swap ! ;
