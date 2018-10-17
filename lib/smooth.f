750504c20ffa1d50b311abb5f298c8bfaac00f5b
Unknown <roger.levy@gmail.com>
2018-10-16 22:22:04 -0400

    1.8.7 tweak how smooth.f works.
    asdf

--- lib/smooth.f
+++ lib/smooth.f
@@ -3,8 +3,7 @@ defer cb
 : each-data  ( xt adr -- )  ( ... adr -- ... )
     swap is cb  begin @ ?dup while  dup >r  cell+ cb   r> repeat ;
 : add-data  ( adr -- )  dup @ here rot ! , ;
-: @execute  @ execute ;
-: :smooth  smoothers add-data here 0 , :noname swap ! ;
-: smooth   ['] @execute smoothers each-data ;
-: smooths  ( objlist )  each> smooth ;
+: @execute  dup @ execute ;
+: +smoother  ( xt -- )  smoothers add-data , ;
+: smooths  ( objlist )  each> ['] @execute smoothers each-data ;
 : lerp!  ( adr dest-val factor -- )  >r  over @ swap r> lerp swap ! ;
--- ramen.f
+++ ramen.f
@@ -1,6 +1,6 @@
 [undefined] [ramen] [if] 
 include afkit/ans/version.f
-#1 #8 #6 [version] [ramen]
+#1 #8 #7 [version] [ramen]
 cr .( Loading Ramen... ) \ "
 include ramen/base.f          \ gilded
 [in-platform] sf [if]
