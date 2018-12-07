54b6e3c57a1fbfce5bc364f1b6877308aad79258
Unknown <roger.levy@gmail.com>
2018-12-07 17:37:02 -0500

    add all the things

---
+++ kitconfig.f
@@ -0,0 +1,11 @@
+512 cells constant object-maxsize
+
+: initial-res  320 240 ;
+: initial-scale 4 ;
+: initial-pos  100 0 ;
+
+\ true constant allegro-debug
+true constant allegro-audio
+true constant allegro-debug
+include afkit/plat/sfwin32.f
+ 
---
+++ loader.f
@@ -0,0 +1,15 @@
+warning off
+true constant dev
+true constant HD
+true constant opengl
+include ramen/ramen.f
+\ include ws1/ws1.f
+\ ui off
+
+fs on
+ide
+include ramen/stdpack.f
+include ramen/lib/tiled/tiled.f
+
+ld main
+
