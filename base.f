855a72ca70642d6d9e5029ee966e2fa8f62ef1ce
Unknown <roger.levy@gmail.com>
2018-12-07 17:37:51 -0500

    make venery external

--- base.f
+++ base.f
@@ -11,23 +11,23 @@ include afkit/afkit.f  \ AllegroForthKit
 [undefined] LIGHTWEIGHT [if]
 include afkit/dep/zlib/zlib.f
 [then]
-include ramen/structs.f
+include ramen/structs.f cr .( Loaded structs lex... ) \ "
 include ramen/fixops.f
 include afkit/plat/sf/fixedp.f \ must come after fixops.  we need fixed-point literals ... it's unavoidable
-include ramen/res.f
-include ramen/venery/venery.f
+include ramen/res.f     cr .( Loaded fixed-point... ) \ "
+include venery/venery.f cr .( Loaded Venery... ) \ "
 
 \ Assets
-include ramen/assets.f
-include ramen/image.f
-include ramen/font.f
-include ramen/buffer.f
-include ramen/sample.f
+include ramen/assets.f  cr .( Loaded assets framework... ) \ "
+include ramen/image.f   cr .( Loaded image module... ) \ "
+include ramen/font.f    cr .( Loaded font module... ) \ "
+include ramen/buffer.f  cr .( Loaded buffer module... ) \ "
+include ramen/sample.f  cr .( Loaded sample module... ) \ "
 
 \ Higher level facilities
-include ramen/obj.f
-include ramen/publish.f
-include ramen/draw.f
+include ramen/obj.f     cr .( Loaded objects module... ) \ "
+include ramen/publish.f cr .( Loaded publish module... ) \ "
+include ramen/draw.f    cr .( Loaded draw module... ) \ "
 
 redef off  \ from here on fields only defined if not previously defined
 
