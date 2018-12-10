146c579779f6127941175e1fb651e2e3b31bbbcd
Roger Levy <roger.levy@gmail.com>
2018-12-10 14:43:08 -0500

    Add stack diagrams to assets.f

--- assets.f
+++ assets.f
@@ -8,24 +8,24 @@ defer initdata ( - )
 
 create assets 1000 *stack drop
 
-: register  ( reloader-xt asset - ) dup assets push  ! ;
-: -assets  assets vacate ;
-: srcfile  cell+ ;
+: register ( reloader-xt asset - ) dup assets push  ! ;
+: -assets ( - ) assets vacate ;
+: srcfile ( - ) cell+ ;
 
 \ The first cell in every asset is a reloader XT.
 : reload  ( asset - )  ( asset - )  dup @ execute ;
 
 \ Note: Don't worry that the paths during development are absolute;
 \ in publish.f, all asset paths are "normalized".
-: findfile
+: findfile ( path c - path c )
     locals| c fn |
     fn c 2dup file-exists ?exit
     including -name #1 + 2swap strjoin 2dup file-exists ?exit
     true abort" File not found" ;
 
 : defasset  ( - <name> )  struct  /assetheader lastbody struct.size ! ;
-: .asset  srcfile count dup if  type  else  2drop  then ;
-: .assets  assets each> cr .asset ;
+: .asset  ( asset - ) srcfile count dup if  type  else  2drop  then ;
+: .assets  ( - ) assets each> cr .asset ;
 : loadtrig  ( xt - )  here swap , assets push ;
 
 \ ------------------------------------------------------------------------------
