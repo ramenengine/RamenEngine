4aa2fe6ae1970ce5e655932f5842aa62df53c93c
Unknown <roger.levy@gmail.com>
2018-12-24 15:43:08 -0500

    add QUEST (savestate)

--- sample/zelda/vars.f
+++ sample/zelda/vars.f
@@ -1,18 +1,29 @@
-( global variables )
+( global variables and savestate )
+
+: qvar  dup constant cell+ ;
+: qfield  over constant + ;
+
+create quest  64 kbytes /allot
+quest
+    \ #16 qfield player
+    2 cells qfield coords  3 3 coords 2!
+    qvar room#
+    qvar world#
+    qvar in-cave
+    qvar tempx  qvar tempy
+    \ maxsize qfield linkstate
+    
+drop
+
 
 16 constant #cols
 11 constant #rows
-create coords 3 , 3 , 
-variable room#
-0 value ^map
 create bg object 
 create cam object 
 create hud object
 create minimap object
 create link object
 : p1  link ;
-variable in-cave
-create tempx 0 , 0 ,
 #1
 bit #weapon
 drop
--- sample/zelda/zelda.f
+++ sample/zelda/zelda.f
@@ -7,7 +7,18 @@ include sample/zelda/items.f
 include sample/zelda/link.f
 include sample/zelda/enemies.f
 
-( overworld scene! )
+( overworld map data )
+16 8 world overworld  overworld
+$FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , 
+$FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , 
+$FF , $FF , $11 , $10 , $01 , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , 
+$FF , $FF , $FF , $00 , $31 , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , 
+$FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , 
+$FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , 
+$FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , 
+$FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , $FF , 
+
+
 : /bg   bg as /tilemap 256 256 w 2! ;
 : /cam  cam as act> x 2@ bg 's scrollx 2! ;
 : /hud  hud as bg 's x @ 0 x 2! draw> black 256 64 rectf ;
@@ -24,20 +35,38 @@ include sample/zelda/enemies.f
 ;
 : /minimap  minimap as 16 16 hud situate draw> red urhere grey mapgrid ;
 
+: after>  ( n - <code> )  r> code> swap after ;
 
-/bg /cam /hud /minimap 
-
-link as hidden on 
-
-curtain-open
+:listen
+    s" player-left-room" occurred if
+        in-cave @ if
+            overworld return
+            in-cave off
+            0 s" player-exited-cave" occur
+        else 
+            x @ 0 <= if gw 256 16 - x ! ;then
+            x @ 256 16 - >= if ge 0 x ! ;then
+            y @ 64 16 + <= if gn 256 16 - y ! ;then
+            y @ 256 16 - >= if gs 64 16 + y ! ;then
+        then
+    ;then
+    s" player-entered-cave" occurred if
+        ( x y ) in-cave on
+    ;then
+;
 
 : adventure
+    zelda-step
+    /bg /cam /hud /minimap
+    curtain-open
+    link as hidden on
+    64 after>
     /link  64 96 x 2!  
     link from *orb
     64 128 at *statue
 ;
 
-' adventure 64 after
+adventure
 
 \ show-cboxes
 
