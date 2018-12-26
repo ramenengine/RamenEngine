77f1061fdeec9e2e4f506d8907e7230784726627
Unknown <roger.levy@gmail.com>
2018-12-25 22:05:35 -0500

    add quest vars and a few item attributes

--- sample/zelda/vars.f
+++ sample/zelda/vars.f
@@ -1,18 +1,43 @@
-( global variables )
+( global variables and savestate )
+
+: qvar  dup constant cell+ ;
+: qfield  over constant + ;
+
+create quest  64 kbytes /allot
+quest
+    \ #16 qfield player
+    2 cells qfield coords  3 3 coords 2!
+    qvar room#
+    \ qvar world#
+    qvar in-cave
+    qvar tempx  qvar tempy
+    \ maxsize qfield linkstate    
+drop
+
 
 16 constant #cols
 11 constant #rows
-create coords 3 , 3 , 
-variable room#
-0 value ^map
+\ create coords 3 , 3 , 
+\ variable room#
+0 value world^
 create bg object 
 create cam object 
 create hud object
 create minimap object
 create link object
 : p1  link ;
-variable in-cave
-create tempx 0 , 0 ,
+\ variable in-cave
+\ create tempx 0 , 0 ,
+
+\ attributes
 #1
 bit #weapon
+drop
+
+\ item types
+: enum  dup constant 1 + ;
+1
+enum #sword
+enum #bomb
+enum #potion
 drop
