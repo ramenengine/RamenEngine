0105aef6672400b18c08fa335d2db2b3d5386cbd
Unknown <roger.levy@gmail.com>
2019-01-07 23:59:57 -0500

    Add directory to loader.f (main.f is blank now)

--- loader.f
+++ loader.f
@@ -8,5 +8,11 @@ ide
 include ws/ws.f               cr .( Loaded Workspace. ) \ "
 ui off
 include ramen/stdpack.f       cr .( Loaded Standard Pack. ) \ "
+
+( Directory )
+: wstest   s" ld ws/test" evaluate ;
+: 3d2019   s" ld sample/3d2019/3d2019" evaluate ;
 gild
+
+( Load main program )
 ld main
--- main.f
+++
@@ -1,3 +0,0 @@
-\ include ws/test.f
-\ include sample/3d2019/3d2019.f
-
