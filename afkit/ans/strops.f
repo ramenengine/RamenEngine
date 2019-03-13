acbe502cfca1c7f8e7b26989392198a7cb66fa94
Unknown <roger.levy@gmail.com>
2019-03-12 23:32:28 -0400

    remove TEMPBUF , add ADDCHAR , enlarge ZBUF

--- afkit/ans/strops.f
+++ afkit/ans/strops.f
@@ -14,10 +14,9 @@ variable >out
   >out @ $outbufs + count
   >out @ 256 + 16383 and >out !
   >s @ 256 - 16383 and >s ! ;
-: tempbuf    >out @ $outbufs +   >out @ 256 + 16383 and >out ! ;
-create zbuf 1024 allot
+create zbuf 65537 allot
 : zstring  ( addr c - zaddr )   zbuf zplace  zbuf ;
-\ : cappend  ( adr c - )   swap dup >r  count + c!  1 r> c+! ;
+: addchar  ( c adr - )   dup >r  count + c!  1 r> c+! ;
 : uncount  ( adr c - adr-1 )   drop 1 - ;
 : strjoin  ( first c second c - first+second c )   2swap s[ +s ]s ;
 \ : input  ( adr c - )   over 1 +  swap accept  swap  c! ;
