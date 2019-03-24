114160ee58d8b1c630dfce9356d9952d69b54663
Unknown <roger.levy@gmail.com>
2019-03-24 17:54:08 -0400

    Only mouse-scroll command history and clear on F2 when REPL is on

--- ws/ws.f
+++ ws/ws.f
@@ -165,8 +165,10 @@ only forth definitions also wsing
     etype ALLEGRO_EVENT_MOUSE_AXES = if
         evt ALLEGRO_MOUSE_EVENT.x 2@ 2p mouse xy!
         { figure ?hover }
-        evt ALLEGRO_MOUSE_EVENT.dz @ 0 > if ws:pageup then
-        evt ALLEGRO_MOUSE_EVENT.dz @ 0 < if ws:pagedown then
+        repl @ if 
+            evt ALLEGRO_MOUSE_EVENT.dz @ 0 > if ws:pageup then
+            evt ALLEGRO_MOUSE_EVENT.dz @ 0 < if ws:pagedown then
+        then
     then
     etype ALLEGRO_EVENT_MOUSE_BUTTON_DOWN = if ?click then
     etype ALLEGRO_EVENT_MOUSE_BUTTON_UP = if { unclick } then
@@ -188,7 +190,8 @@ only forth definitions also wsing
 : ?toggle-ui
     etype ALLEGRO_EVENT_KEY_DOWN = keycode <f10> = and if  ui @ not ui !  then
     etype ALLEGRO_EVENT_KEY_DOWN = keycode <f2> = and if
-        repl @ ui @ or if page repl off ui off else repl on ui on then
+        repl @ if page then 
+        repl @ ui @ or if repl off ui off else repl on ui on then
     then 
 ;
 : (system)   ide-system  ?toggle-ui  ui @ if ui-mouse then ;
