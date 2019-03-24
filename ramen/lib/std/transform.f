0499b39baddecb12e0b0bc79bf49f667bc931244
Unknown <roger.levy@gmail.com>
2019-03-24 16:43:06 -0400

    fix TRANSFORM; rename MCURRENT to MACTIVE and add COMPOSE
    MOUNT and UNMOUNT now work with transform.f properly
    asdf

--- ramen/lib/std/transform.f
+++ ramen/lib/std/transform.f
@@ -2,23 +2,30 @@
 create mstack 16 cells 32 * /allot
 transform: t:m
 variable (m)
-: mcurrent  (m) @ 16 cells - [ 16 cells 32 * #1 - ]# and mstack + ;
-: mtop  (m) @ [ 16 cells 32 * #1 - ]# and mstack + ;
-: tpush ( - )  16 cells (m) +!  mcurrent al_use_transform  mcurrent mtop 16 cells move ; 
-: tpop ( - )  -16 cells (m) +!  mcurrent al_use_transform  mcurrent mtop 16 cells move ;
+: mactive  ( - adr ) (m) @ 16 cells - [ 16 cells 32 * #1 - ]# and mstack + ;
+: mtop  ( - adr ) (m) @ [ 16 cells 32 * #1 - ]# and mstack + ;
+: mget  ( - ) al_get_current_transform mtop 16 cells move ;   
+: tpush ( - )  16 cells (m) +!  mactive al_use_transform  mactive mtop 16 cells move ; 
+: tpop ( - )  -16 cells (m) +!  mactive al_use_transform  mactive mtop 16 cells move ;
 : translate  ( x y - ) 2af mtop -rot al_translate_transform ;
 : scale  ( sx sy - ) 2af mtop -rot al_scale_transform ;
 : rotate  ( angle - ) 1af mtop swap al_rotate_transform ;
 : hshear  ( n - ) 1af mtop swap al_horizontal_shear_transform ;
 : vshear  ( n - ) 1af mtop swap al_vertical_shear_transform ;
-: identity  mtop al_identity_transform ;
-identity tpush
+: identity  ( - ) mtop al_identity_transform ;
+: compose  ( - )  mtop mactive al_compose_transform ;
+: mount   ( - ) tpop mount mget tpush ;
+: unmount   ( - ) tpop unmount mget tpush ;
+
+identity tpush  
 
 ( draw relative )
 : transform  ( - )
+    identity
     sx 2@ scale
     ang @ rotate
     x 2@ [undefined] HD [if] 2pfloor [then] translate
+    compose
 ;
 
 : view  ( - )
@@ -28,7 +35,7 @@ identity tpush
 ;
 
 : view>  ( object - <code> )
-    >{ view tpush } r> call tpop ;
+    >{ view } tpush r> call tpop ;
 
 : transform>  ( object - <code> )
-    >{ transform tpush } r> call tpop ;
+    >{ transform } tpush r> call tpop ;
