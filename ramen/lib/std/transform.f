e7537ef327d7580f466c40cde0365392a885e843
Unknown <roger.levy@gmail.com>
2019-03-29 19:22:14 -0400

    pen's pos is now as expected with TRANSFORM and TRANSFORM>

--- ramen/lib/std/transform.f
+++ ramen/lib/std/transform.f
@@ -2,11 +2,14 @@
 create mstack 16 cells 32 * /allot
 transform: t:m
 variable (m)
+
 : mactive  ( - adr ) (m) @ 16 cells - [ 16 cells 32 * #1 - ]# and mstack + ;
 : mtop  ( - adr ) (m) @ [ 16 cells 32 * #1 - ]# and mstack + ;
 : mget  ( - ) al_get_current_transform mtop 16 cells move ;   
 : tpush ( - )  16 cells (m) +!  mactive al_use_transform  mactive mtop 16 cells move ; 
 : tpop ( - )  -16 cells (m) +!  mactive al_use_transform  mactive mtop 16 cells move ;
+
+( transformation ops )
 : translate  ( x y - ) 2af mtop -rot al_translate_transform ;
 : scale  ( sx sy - ) 2af mtop -rot al_scale_transform ;
 : rotate  ( angle - ) 1af mtop swap al_rotate_transform ;
@@ -19,13 +22,13 @@ variable (m)
 
 identity tpush  
 
-( draw relative )
 : transform  ( - )
     identity
     sx 2@ scale
     ang @ rotate
     x 2@ [undefined] HD [if] 2pfloor [then] translate
     compose
+    0 0 at 
 ;
 
 : view  ( - )
@@ -38,4 +41,4 @@ identity tpush
     { view } tpush r> call tpop ;
 
 : transform>  ( object - <code> )
-    { transform } tpush r> call tpop ;
+    r> swap  at@ 2>r  { transform } tpush  call  tpop  2r> at ;
