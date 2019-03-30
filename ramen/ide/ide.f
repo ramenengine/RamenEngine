611828662a4d8b34c095eb36aad0e4dbc2980b1b
Unknown <roger.levy@gmail.com>
2019-03-29 23:38:30 -0400

    game goes grey instead of dark when repl is on now

--- ramen/ide/ide.f
+++ ramen/ide/ide.f
@@ -282,7 +282,7 @@ create ide-personality
     >display
     ide-personality open-personality
 ;
-: shade  black 0.15 alpha  0 0 at  displaywh rectf  white ;
+: shade  dgrey 0.5 alpha  0 0 at  displaywh rectf  white ;
 : ?rest
     source-id close-file drop
     [in-platform] sf [if]  begin refill while interpret repeat  [then] ; 
